092bcf6837eb18927a30dc479c31d4354968d5af
Samantha Ibasitas <36210345+samanthaibasitas@users.noreply.github.com>
2019-02-26 18:01:54 -0800

    Add files via upload

---
+++ person.cpp
@@ -0,0 +1,60 @@
+#include "person.h"
+#include <string> 
+using namespace std;
+Person::Person()
+{
+	
+}
+string Person::getFirstName()
+{
+    return firstName;
+}
+
+void Person::setLastName(string lName)
+{
+    lastName = lName;
+}
+
+string Person::getLastName()
+{
+    return lastName;
+}
+
+void Person::setFirstName(string fName)
+{
+    firstName = fName;
+}
+
+void Person::setPayRate(float rate)
+{
+    payRate = rate;
+}
+
+float Person::getPayRate()
+{
+    return payRate;
+}
+
+void Person::setHoursWorked(float hours)
+{
+    hoursWorked = hours;
+}
+
+float Person::getHoursWorked()
+{
+    return hoursWorked;
+}
+
+float Person::totalPay()
+{
+    int total;
+    total = hoursWorked * payRate;
+    return total;
+}
+
+string Person::fullName()
+{
+    string fullName; 
+    fullName = firstName + " " +lastName;
+    return fullName;
+}
